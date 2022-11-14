e200b9c8891c22334dbbf2bb3393d9f1ac12f134
CutzuDev <alexfarkas05@gmail.com>
2022-11-14 23:29:47 +0200

    Updates

--- src/pages/Mail.jsx
+++ src/pages/Mail.jsx
@@ -1,9 +1,54 @@
+import {
+  ArrowBack,
+  CheckCircle,
+  Delete,
+  Email,
+  Error,
+  LabelImportant,
+  MoreVert,
+  MoveToInbox,
+  WatchLater,
+} from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import "./Pages.css";
 
 function Mail() {
-  return <div className="">
-    <h1>Teeest</h1>
-  </div>;
+  return (
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+          <IconButton>
+            <MoveToInbox />
+          </IconButton>
+          <IconButton>
+            <Error />
+          </IconButton>
+          <IconButton>
+            <Delete />
+          </IconButton>
+          <IconButton>
+            <Email />
+          </IconButton>
+          <IconButton>
+            <WatchLater />
+          </IconButton>
+          <IconButton>
+            <CheckCircle />
+          </IconButton>
+          <IconButton>
+            <LabelImportant />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Mail;
