e1cc94054f30c93cf49df6d7a22af44257cc651a
CutzuDev <alexfarkas05@gmail.com>
2022-11-16 22:11:56 +0200

    Updates + Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVM8Pl7wtfP5_DKbQIxqAbVCoqKkn7UT4",
+  authDomain: "clone-abe65.firebaseapp.com",
+  projectId: "clone-abe65",
+  storageBucket: "clone-abe65.appspot.com",
+  messagingSenderId: "155009691883",
+  appId: "1:155009691883:web:7de45b01ddb7121b09ceae",
+  measurementId: "G-6BS49GWV6M",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
