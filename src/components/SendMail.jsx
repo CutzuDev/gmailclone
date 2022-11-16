8df0ee06d482a90867bac3e4eb07e316e8651bf0
CutzuDev <alexfarkas05@gmail.com>
2022-11-16 18:19:07 +0200

    Updates

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -1,21 +1,50 @@
 import { Close } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+
 import "./Components.css";
 
 function SendMail() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="sendMail">
       <div className="sendMail__header">
         <h3>New Message</h3>
         <Close className="sendMail__close" />
       </div>
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="to"
+          placeholder="To"
+          type="text"
+          {...register("to", { required: true })}
+        />
+        <input
+          name="subject"
+          placeholder="Subject"
+          type="text"
+          {...register("subject", { required: true })}
+        />
+        <input
+          name="message"
+          placeholder="Message..."
+          type="text"
+          className="sendMail__message"
+          {...register("message", { required: true })}
+        />
 
         <div className="sendMail__options">
-          <Button>Send</Button>
+          <Button className="sendMail__send">Send</Button>
         </div>
       </form>
     </div>
